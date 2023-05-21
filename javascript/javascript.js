19bae227016b69cc364bf9d3c091a4c6122bfd9b
malesgz <malenagonzalez959@gmail.com>
2023-05-21 14:05:59 -0300

    Array y algunos métodos

---
+++ javascript/javascript.js
@@ -0,0 +1,31 @@
+// Arreglos en Javascript.
+
+let persona = ["Malena", "Analuz", "Gonzalez"];
+
+// Método length: nos ayuda a saber cuantos elementos tiene nuestro array.
+// console.log(persona.length);
+
+// Método Join: nos permite unir todos los elemetos junto a una cadena de texto.
+// let cadenaTexto = persona.join("-");
+// console.log(cadenaTexto);
+
+// Método de pop:(permite eliminar un ultimo elemento del array) y push: (permite colocar un último elemento).
+// persona.pop();
+// persona.push("Gonzalez");
+
+// Método shift:(eliminar el elemento que este primero en el array) y unshift:(permite agregar un elemento antes de todos los demás).
+// persona.shift();
+// persona.unshift("Malena");
+
+// Método concat: (permite unir arrays)
+// let persona2 = ["Lara", "Rita", "Mariel", "Gonzalez"];
+// let juntarPersonas = persona.concat(persona2);
+
+// Métodos Sort: (ordena los elementos del array alfabeticamente y solo funcionan con cadenas de texto, no con números) y Reverse: (ordena los elementos del array de atrás hacia delante).
+// persona.sort();
+// persona.reverse();
+
+let numero1 = 10;
+let numero2 = 5;
+let resultado = numero1 + numero2;
+document.write(resultado);
